2b1fad96cf9d46da473364820144a1351cf4cc2d
beltranaugusto <beltranliscano@gmail.com>
2023-03-21 22:51:30 -0400

    Initial commit, finished project

--- src/js/component/Task.jsx
+++ src/js/component/Task.jsx
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 
 const Task = (props) => {
 
+    const {task, id, done, doneTask, deleteTask, spinner} = props
+
     const [icon, setIcon] = useState(false)
     const showIcon = () => {
         setIcon(true)
@@ -10,23 +12,24 @@ const Task = (props) => {
         setIcon(false)
     }
 
-    // Busco borrar el componente cambiando el State de su componente padre
-    // Como comunicar estados entre componentes?
-
-        // Pasarle la funcion de borrar la tarea a este componente hijo mediante props
-
 	return (
         <>
             <div
                 onMouseOut={hideIcon} 
                 onMouseOver={showIcon} 
-                className={"m-2 mb-3 fs-4 fw-light border-bottom d-flex justify-content-between"}
+                className={"p-1 m-1 fs-4 fw-light d-flex justify-content-between rounded px-3 " + (done === true ? "done": "pending")}
             >
-                <div>{props.task}</div>
-                <i
-                    onClick={() => props.deleteTask(props.id)}
-                    className={"fa-regular fa-trash-can my-auto o-0" + (icon === true ? "o-100": "")}
-                ></i>
+                <div>{task}</div>
+                <div className={"my-auto o-0" + (icon === true ? "o-100": "")}>
+                    <i 
+                        className="fa-solid fa-check mx-3"
+                        onClick={() => doneTask(id)}
+                    ></i>
+                    <i
+                        onClick={() => deleteTask(id)}
+                        className={"fa-regular fa-trash-can"}
+                    ></i>
+                </div>
             </div>
         </>
 	);
--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,31 +1,117 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Task from "./Task.jsx";
+
+// Clase del 7/12/2022, resolvimos el problema que estaba pasando ajustando el orden el que el getTask era llamado.
+// El problema es que no coincidia el recibir la data y la actualizacion del componente.
+// Tengo que revisar los comentarios para ver los cambios que se hicieron y por qué.
+// Tambien tengo que hacer las verificaciones de la respuesta recibida bien, y a partir de eso, ejecutar el codigo que quiero.
+// Porque estaba haciendo el fetch, e indenpendientemente de lo que recibia, si lo recibia o no, ejecutaba lo que ejecutaba.
 
 const TodoList = () => {
 
     const [inputValue, setInputValue ] = useState('');
     const [error, setError] = useState(false);
+    const [errortext, setErrortext] = useState(false);
     const [tasks, setTask] = useState([]);
+    const [spinner, setSpinner] = useState(false);
     
+    // getTasks is the function that makes a GET request to the API to get the tasks.
+
+    const getTasks = async () => {
+        setSpinner(true)
+        try {
+                let receivedTasks = await fetch('https://assets.breatheco.de/apis/fake/todos/user/beltran', {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"  
+                }
+                })
+                let data = await receivedTasks.json()
+                setTask(data)
+                setSpinner(false)
+                console.log(data)
+            } 
+        catch (error) {
+                console.log(error)
+            }
+    }
+
+    // modifyTasks is the function that makes a PUT request to the API to modify the task list on the database to whatever task list we send it as argument in the function call.
+
+    const modifyTasks = async (modifiedTasks) => {
+        //setSpinner(true)
+        console.log(modifiedTasks)
+        try {
+            let response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/beltran', {
+                method: "PUT",
+                body: JSON.stringify(modifiedTasks),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            console.log(response)
+            if (response.status === 400){
+                setError(true)
+                setErrortext("The to-do list cannot be empty.")
+            }
+            getTasks();
+            //SsetSpinner(false)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    // addTask, deleteTask and doneTask work on a local level.
+    // They do the logic required to add, delete or mark as done a task.
+    // Then it calls the modifyTask function with the updated task list to reflect those changes in the API database.
+    // After that, getTasks() gets called to update the component with the recently modified task list.
+
+    //setError(false) gets called at the beginning of each function to clean the error message if there was one.
+
     const addTask = (event) => {
+        setError(false)
         if (event.key === "Enter"){
             if (inputValue != ""){
-                setTask(prev => [...tasks, inputValue])
+                modifyTasks([...tasks, {label: inputValue, done: false}])
+                //getTasks();
                 setInputValue("")
-                setError(false)
+                
             } else {
                 setError(true)
+                setErrortext("The task cannot be empty.")
             }
         } 
     }
 
     const deleteTask = (id) => {
+        setError(false)
         let newTasks = tasks.filter((item, index) => id != index)
-        setTask(newTasks)
+        modifyTasks(newTasks)
+        getTasks();
+        
     }
 
+    const doneTask = (id) => {
+        setError(false)
+        
+        if (tasks[id].done === true){
+            tasks[id].done = false
+        } else {
+            tasks[id].done = true
+        }
+
+        let newTasks = tasks
+        modifyTasks(newTasks);
+        getTasks();
+    }
+
+    useEffect(() => {
+        getTasks();
+    }, [])
+
 	return (
-		<div className="max-min container d-flex flex-column justify-content-center align-items-center border rounded">
+		<div className="max-min container d-flex flex-column justify-content-center align-items-center border rounded pb-2">
             <div className="display-4 border-bottom mt-2">To-Do List</div>
             <div className="form-group m-3">
                 <input
@@ -36,9 +122,15 @@ const TodoList = () => {
                     value={inputValue}
                 />
             </div>
-            <div className="w-100">
-                {error === true ? <div className="alert alert-danger">The task cannot be empty.</div> : null}
-                {tasks.map((item, index) => <Task key={index} id={index} task={item} deleteTask={deleteTask}/>)}
+            <div className="w-100 d-flex flex-column">
+
+                {error === true ? <div className="text-center fs-4 mb-3 fw-light">{errortext}</div> : null}
+
+                {spinner === true 
+                    ?<div class="spinner-border text-secondary m-auto mb-3" role="status"><span class="sr-only">Loading...</span></div>
+                    :tasks.map((item, index) => <Task key={index} id={index} done={item.done} task={item.label} deleteTask={deleteTask} doneTask={doneTask}/>)
+                }
+                
             </div>
         </div>
 	);
